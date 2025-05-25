4ac3a0b003ffb7d54d994ecc546be56fba805398
KEVAL PANCHAL <kevalpanchal0602@gmail.com>
2025-05-25 22:27:39 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => {
+  // command === 'serve' for dev, 'build' for production
+  return {
+    base: command === 'build' ? "/your-repo-name/" : "/",
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
